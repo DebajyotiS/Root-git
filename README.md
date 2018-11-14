9d5e0ee817997c9459bc6df4a91aa611723b9ae9
DebajyotiS <debajyoti.sg@gmail.com>
2018-11-14 14:58:01 +0530

    Roofit and readme.md update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
-# test This repo consists of root codes for generating the e-e+ peak from a monte carlo simulated data file
+# test 
+#This repo consists of root codes for generating the e-e+ peak from a monte carlo simulated data file
+#Edit 1: Now using roofit to fit data. Original fit functions are still there in the comments below the code.
 
--- zdecay.C
+++ zdecay.C
@@ -5,11 +5,21 @@
 #include <TCanvas.h>
 #include <TLorentzVector.h>
 #include <TFormula.h>
+#include <TF1.h>
+#include <TList.h>
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooDataHist.h"
+#include "RooGaussian.h"
+#include "RooExponential.h"
+#include "RooFitResult.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TAxis.h"
+#include <RooCBShape.h>
+using namespace RooFit ;
 
 
-
-
-// using namespace std;
 
 class particles{
 public:
@@ -72,31 +82,23 @@ private:
 };
 
 
-
 //* This is the comparing function. It takes two values and returns False/True based on the expression used.
 bool compare_trans(particles &particle1, particles &particle2){
     return particle1.getPt() > particle2.getPt();
    }
 
+
 //* This is the main loop.
 void zdecay::Loop()
 {
 
-   //* Creating a vector of objects of class particles.
-   // TCanvas c1("c1","Histogram here",600,800);
-   cout << "Begin" << endl;
-   // int verbose_level = 1;
-   // int need = 0;
-
+   
+   // cout << "Begin" << endl;
    if (fChain == 0) return;  
-   // TCanvas c1("c1","c1",800,1000);
-   TCanvas *c1 = new TCanvas("c1","Plots",200,10,900,900);
-
+   
    TH1F *h1 = new TH1F("h1","Histogram;Mass;Count",150,0,600);
    // h1.FillRandom("gaus",250000);
    
-
-
    Long64_t nentries = fChain->GetEntriesFast();
    cout<<"Total entries "<<nentries<<endl<<endl;
    Long64_t nbytes = 0, nb = 0;
@@ -106,13 +108,13 @@ void zdecay::Loop()
       //if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);
       nbytes += nb;
-      // cout << "processing event "<<jentry <<endl;
+      
+      //* Creating a vector of objects of class particles.
       vector<particles> electrons;
-      // cout<<"event no "<<jentry<<" eta of first electron "<<nEle<<endl;
+  
       
-      
-      // Acquisition loop. If the event has at least one electron (or particle) then it makes an electron object
-      // which is inserted into the vector - electrons
+            // Acquisition loop. If the event has at least one electron (or particle) then it makes an electron object
+            // which is inserted into the vector - electrons
 
       if(nEle>1) {
          for (int i=0 ;i<nEle; i++){
@@ -120,78 +122,105 @@ void zdecay::Loop()
             electrons.push_back(electron);
          }
          std::sort(electrons.begin(),electrons.end(), compare_trans);
+         
+         // Contructing the four vector for the first two particles with highest Pt - Summing them in V3 for display.
+         TLorentzVector v1;
+         v1.SetXYZT(electrons[0].getPx(),electrons[0].getPy(),electrons[0].getPz(),electrons[0].getE());
+         TLorentzVector v2;
+         v2.SetXYZT(electrons[1].getPx(),electrons[1].getPy(),electrons[1].getPz(),electrons[1].getE());
+         TLorentzVector v3 = v1+v2;
+         h1 -> Fill(v3.M());
+      
+               }
+      
+                                                    }
 
-      // Contructing the four vector for the first two particles with highest Pt - Summing them in V3 for display.
-      TLorentzVector v1;
-      v1.SetXYZT(electrons[0].getPx(),electrons[0].getPy(),electrons[0].getPz(),electrons[0].getE());
-      TLorentzVector v2;
-      v2.SetXYZT(electrons[1].getPx(),electrons[1].getPy(),electrons[1].getPz(),electrons[1].getE());
-      TLorentzVector v3 = v1+v2;
-      // if (v3.M()>60.0 & v3.M() <140.0)
-      h1 -> Fill(v3.M());
-      // if (v3.M()>60.0 & v3.M() <140){
-      //    need += 1;
+      // for ( int i = 14; i<=28; i++){
+      //    double a = h1->GetBinCenter(i);
+      //    h1 -> SetBinContent(i,100.);
       // }
-      // //if (verbose_level) cout << v3.M() <<endl;
-      // if(verbose_level)cout << v3.M() <<endl;
-      //delete electrons;
-      //cout << "Pt,Phi,Eta for the top 2 highest Pt:"<<endl<<endl;
-      // if (v3.M()>0) cout <<v3.M()<<jentry<<endl;
-      // h1.Fill(v3.M(),1);
-      // cout << v3.M()<<endl;
-      // h1.Fill(v3.M());
-      // if (verbose_level > 1){
-      //    for(int i=0;i<2;i++){   
-      //       cout << electrons[i].getPt()<<" "<< electrons[i].getphi()<<" "<< electrons[i].geteta()<<" "<<electrons[i].getcharge()<<endl;
-      //    }  
-      //    cout << endl<< endl<<"Four vectors(E,Px,Py,Pz)"<<endl<<endl;
-      //    cout << "   e-"<<"          "<<"e+"<<"        "<<"sum"<<endl;
+      
 
-      //    for (int i=0;i<4;i++){
-      //    cout << v1[i] <<"    " << v2[i] <<"    "<<v3[i]<< endl;
-      //    }
-      // }
-      }
+   RooRealVar x("x","mass",0.,1000.);
+   // x.setRange("Range1",35.75,50.13) ;
+   x.setRange("Range2",110.101,200.) ;
+   RooRealVar lambda("lambda", "slope", -0.001, -1., 1.);
+   RooExponential expo("expo", "expo", x, lambda);
+   RooExponential expo1("expo1", "expo", x, lambda);
+   RooDataHist dh("dh","e-e+ peak histo",x,Import(*h1)) ;
+   // RooFitResult* r = 
+   RooExtendPdf background("background","Side Bands",expo,x,"FULL") ;
+   RooFitResult* r = expo.fitTo(dh,Range("Range2"), Save(kTRUE)) ;
+   r -> Print();
+   
+   RooPlot* frame = x.frame() ;
+   RooPlot* frame1 = x.frame() ;
 
       
-      // /cout <<endl<<endl;
-      // cout << v3.M()<< endl;
-      // cout <<endl;
+   dh.plotOn(frame,DrawOption("B"),DataError(RooAbsData::None),FillColor(kGray));
+   expo.plotOn(frame);
+   // background.plotOn(frame,LineStyle(kDashed),LineColor(kRed));
+   
+
+   RooDataSet *h2 = expo.generate(x,2500);
+   
+   
+   h2-> plotOn(frame1, LineColor(kBlue));
+   
+   TH1* h2_hist = h2->createHistogram("h2_hist", x);
+   RooDataHist dh2("dh2","e-e+ peak histo",x,Import(*h2_hist)) ;
+   RooFitResult* r2 = expo1.fitTo(dh2, Save(kTRUE)) ;
+   r2 -> Print();
+   
+   RooPlot *frame3 = x.frame();
+   dh2.plotOn(frame3);
+   expo1.plotOn(frame3);
+   
+   TCanvas* c = new TCanvas("canvas","canvas",800,800) ;
+   c -> Divide(2,2);
+   c -> cd(1);
+   frame -> Draw();
+   c -> cd(2);
+   frame1 -> Draw();
+   c -> cd(3);
+   frame3 -> Draw();
+}
+
+
+
+
+
+
+
+
+
+      // Old fit functions and saving routine
+
+      //! The crystalbal+expo fit routine
+      // TF1 *fit = new TF1("fit", "crystalball(0) + expo(5)", 0., 1000.);
+      // fit ->SetParameters(160.0,90.0,9.0,1.0,220.0, 0.01);
+      // for (Int_t i = 0; i < fit->GetNpar(); i++) std::cout << i << " : " << fit->GetParName(i) << std::endl;
+      // h1->Fit("fit", "", "", 18., 200.);
       
-      } //end jentry loop
-      // h1.Draw();
 
-      TF1 *fit = new TF1("fit", "[0]*exp(-0.5*pow((x-[1])/[2],2)) + [3]*exp(-[4]*x)", 0., 1000.);
-      fit -> SetNpx(10000);
-      fit ->SetParNames("amp_gauss", "Mean_Gaus", "sigma_Gaus", "amp_exp", "exp_arg");
-      fit ->SetParameters(110.0, 90.0, 10.0, 220.0, 0.01);
-      // fit.Draw();
+      // TF1 *fline = new TF1("fline","expo(0)",18.,550.);
+      // fline->SetParameters(220.,-0.01);
+   
+      // h1->Fit("fline","","",18.,65.);
+      // h1->Fit("fline","","",105.,600.);
+      
 
-      h1->Fit("fit", "", "", 18., 150.);
+      //! This part is to create a file and save the plots.
 
-      TFile f("Histo.root","recreate");
-      h1 -> Write();
-      fit -> Write();
-      f.Close();
-      c1 -> cd();
-      h1 ->Draw();
-      TPad *newpad=new TPad("newpad","a transparent pad",0,0,1,1);
-      newpad -> SetFillStyle(4000);
-      newpad ->Draw();
-      newpad ->cd();
-      TPaveLabel *title = new TPaveLabel(0.1,0.5,0.3,0.3,"Mass = 91.9155");
-      title->SetFillColor(0);
-      title->SetTextFont(52);
-      title->Draw();
-
-      // h1.Print("all"); > histo.txt
-      // cout << need<<endl;
-      cout << "end";
-      // TBrowser b;
-      // h1.Draw();
-   }
-
-
+      // TFile f("Histo.root","recreate");
+      // h1 -> Write();
+      // fit -> Write();
+      // f.Close();
+      // c1 -> cd();
+      // h1 ->Draw();
+      
 
       
-    
+      
+     
+   
